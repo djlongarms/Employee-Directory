b19bb8b0b587d2883aab7474d46b058dfa1b9619
Nick Palmer <ylnickpalmer@gmail.com>
2021-04-08 18:55:05 -0700

    wrote code for the application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import ReactTable from 'react-table-v6'
+import axios from 'axios'
+import 'react-table-v6/react-table.css'
 
-function App() {
+const App = () => {
+  const [userState, setUserState] = useState({
+    users: [],
+    columns: [
+      {
+        Header: 'name',
+        accessor: 'name',
+        filterable: true
+      },
+      {
+        Header: 'email',
+        accessor: 'email',
+        filterable: true
+      },
+      {
+        Header: 'phone',
+        accessor: 'phone',
+        filterable: true
+      }
+    ]
+  })
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api?results=20')
+      .then(({ data: { results } }) => {
+        const users = results.map(user => ({
+          name: `${user.name.first} ${user.name.last}`,
+          email: user.email,
+          phone: user.phone
+        }))
+        setUserState({ ...userState, users })
+      })
+      .catch(err => console.error(err))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ReactTable
+      data={userState.users}
+      columns={userState.columns}
+    />
+  )
 }
 
-export default App;
+export default App
